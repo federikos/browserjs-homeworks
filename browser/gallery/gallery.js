d2d23d4fb497c07df4a91d2a97437e47c511c506
Fiodar Savutsin <federikos@bk.ru>
2019-05-28 16:09:20 +0300

    add homework 1.1 Gallery

---
+++ browser/gallery/gallery.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const currentPhoto = document.querySelector('#currentPhoto');
+const prevBtn = document.querySelector('#prevPhoto');
+const nextBtn = document.querySelector('#nextPhoto');
+const imgs = [
+    'i/breuer-building.jpg',
+    'i/guggenheim-museum.jpg',
+    'i/headquarters.jpg',
+    'i/IAC.jpg',
+    'i/new-museum.jpg'
+];
+let i = -1;
+const lastI = imgs.length - 1;
+
+const changeImgAttributes = (index) => {
+    currentPhoto.setAttribute('src', imgs[index]);
+    currentPhoto.setAttribute(
+        'alt', imgs[index]
+        .split('')
+        .splice(2, (imgs[index].length - 6))
+        .join('')
+    );
+}
+
+const prevBtnClickHandler = () => {
+    if (i <= 0) {
+        i = lastI;
+    } else {
+        i--;
+    }
+    changeImgAttributes(i);
+}
+
+const nextBtnClickHandler = () => {
+    if (i === -1 || i === lastI) {
+        i = 0;
+    } else {
+        i++;
+    }
+    changeImgAttributes(i);
+}
+
+prevBtn.addEventListener('click', prevBtnClickHandler);
+nextBtn.addEventListener('click', nextBtnClickHandler);
