2db1ad29dfeed5f951643129d7e097e416c051d6
federikos <federikos@bk.ru>
2019-06-05 00:12:07 +0300

    fix homework 1.2 Dropdown: remove unnecessary code

--- html-element-collection/dropdown-menu/js/dropdown.js
+++ html-element-collection/dropdown-menu/js/dropdown.js
@@ -1,11 +1,5 @@
 'use strict';
 
 const wrapperDropdown = document.querySelector('.wrapper-dropdown');
-const listItemCollection = wrapperDropdown.querySelectorAll('li');
 const toggleClassActive = () => wrapperDropdown.classList.toggle('active');
-
 wrapperDropdown.addEventListener('click', toggleClassActive);
-
-listItemCollection.forEach((li) => {
-  li.addEventListener('click', toggleClassActive);
-});
