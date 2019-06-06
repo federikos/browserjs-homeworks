8f29c946b5e1bf26d909eb2bb8291b4697daa110
federikos <federikos@bk.ru>
2019-06-06 17:34:04 +0300

    replace direct reference with 'this'

--- event-object/skateboard-gallery/js/gallery.js
+++ event-object/skateboard-gallery/js/gallery.js
@@ -4,10 +4,10 @@ const currentImg = document.querySelector('.gallery-view');
 const links = document.querySelectorAll('.gallery-nav a');
 
 for(const link of links) {
-    link.addEventListener('click', (event) => {
+    link.addEventListener('click', function(event) {
         event.preventDefault();
         links.forEach((a) => a.classList.remove('gallery-current'));
         link.classList.add('gallery-current');
-        currentImg.setAttribute('src', link.href);
+        currentImg.setAttribute('src', this.href);
     });
 }
