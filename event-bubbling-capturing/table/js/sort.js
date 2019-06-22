be337862b4768a46258c72fe184884a02edb8399
federikos <federikos@bk.ru>
2019-06-22 18:03:09 +0300

    add homework 3.2 Table

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if(event.target.classList.contains('prop__name')) {
+    const thDataset = event.target.dataset;
+    const tableDataset = event.currentTarget.dataset;
+    thDataset.dir = thDataset.dir ? -thDataset.dir : 1;
+    tableDataset.sortBy = thDataset.propName;
+    sortTable(tableDataset.sortBy, thDataset.dir);
+  }
 }
