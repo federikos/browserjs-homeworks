6f21a17eb67aabf2bbf4e7844854925d8d803dff
federikos <federikos@bk.ru>
2019-06-22 20:34:45 +0300

    add homework 3.2 Shop

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,8 @@
 'use strict';
+list.addEventListener('click', event => {
+    if(event.target.classList.contains('add-to-cart')) {
+        const title = event.target.dataset.title;
+        const price = event.target.dataset.price;
+        addToCart({title, price});
+    }
+});
