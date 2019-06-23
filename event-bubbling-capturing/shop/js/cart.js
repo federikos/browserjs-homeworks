93a3a76b36d71ff836ba13fd89fd061033e36ef6
federikos <federikos@bk.ru>
2019-06-23 10:40:41 +0300

    add preventDefault()

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,6 +1,7 @@
 'use strict';
 list.addEventListener('click', event => {
     if(event.target.classList.contains('add-to-cart')) {
+        event.preventDefault();
         const title = event.target.dataset.title;
         const price = event.target.dataset.price;
         addToCart({title, price});
