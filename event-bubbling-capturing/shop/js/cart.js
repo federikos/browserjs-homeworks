163524c5eab3eac249755b6edec18c31f650d74a
federikos <federikos@bk.ru>
2019-06-26 19:56:08 +0300

    tie code to DOMContentLoaded for more reliability

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,9 +1,11 @@
 'use strict';
-list.addEventListener('click', event => {
-    if(event.target.classList.contains('add-to-cart')) {
-        event.preventDefault();
-        const title = event.target.dataset.title;
-        const price = event.target.dataset.price;
-        addToCart({title, price});
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    list.addEventListener('click', event => {
+        if(event.target.classList.contains('add-to-cart')) {
+            event.preventDefault();
+            const title = event.target.dataset.title;
+            const price = event.target.dataset.price;
+            addToCart({title, price});
+        }
+    });
 });
