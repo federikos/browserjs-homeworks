71fa2414767233d43e44af4b0769d15edee2473a
federikos <federikos@bk.ru>
2019-06-27 00:08:39 +0300

    dd homework 3.4 Counter

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counter = document.querySelector('.counter');
+    const errors = document.querySelector('output.errors');
+
+    const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+    window.addEventListener('beforeunload', event => {
+        event.currentTarget.close(1000, 'Пользователь закрыл страницу');
+    });
+    connection.addEventListener('message', event => {
+        const data = JSON.parse(event.data);
+        counter.innerText = data.connections;
+        errors.value = data.errors;
+    });
+});
