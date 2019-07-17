30d05254e29b817ec09325c98e487fd4cd3ac9d7
federikos <federikos@bk.ru>
2019-07-17 20:49:59 +0300

    add homework 4.3 Comments

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -2,28 +2,46 @@
 
 function showComments(list) {
   const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+  list.forEach(comment => commentsContainer.appendChild(createComment(comment)));
+}
+
+function removeChildren(el) {
+  while (el.lastChild) {
+    el.removeChild(el.lastChild);
+  }
+}
+
+function createNode(elName, elClass, children) {
+  const el = document.createElement(elName);
+  el.classList.add(elClass);
+
+  if (typeof children === 'string') {
+    el.textContent = children;
+  }
+
+  if (children instanceof Array) {
+    children.forEach(child => el.appendChild(child));
+  }
+
+  return el;
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+  const wrapper = document.querySelector('.comment-wrap').cloneNode(true);
+  wrapper.querySelector('.photo').setAttribute('title', comment.author.name);
+  wrapper.querySelector('.avatar').setAttribute('style', `background-image: url('${comment.author.pic}')`);
+  removeChildren(wrapper.lastElementChild);
+  const commentBlock = wrapper.querySelector('.comment-block');
+  commentBlock.appendChild(createNode('p', 'comment-text', comment.text));
+  commentBlock.appendChild(createNode('div', 'bottom-comment', [
+    createNode('div', 'comment-date', new Date(comment.date).toLocaleString('ru-Ru')),
+    createNode('ul', 'comment-actions', [
+      createNode('li', 'complain', 'Пожаловаться'),
+      createNode('li', 'reply', 'Ответить'),
+    ]),
+  ]));
+
+  return wrapper;
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
