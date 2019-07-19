5675cb07e1f4ef3d34b8ffb29646ee472de77298
federikos <federikos@bk.ru>
2019-07-20 01:16:51 +0300

    add homework 4.4 Draw

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const wss = new WebSocket('wss://neto-api.herokuapp.com/draw');
+wss.addEventListener('error', e => {
+  console.error(e.data);
+});
+wss.addEventListener('close', e => {
+  console.log(`${e.code} Websocket-соединение закрыто, причина - ${e.reason}`);
+});
+wss.addEventListener('message', e => {
+  console.log(e.data);
+});
+
+//почему отправка blob работает, а ArrayBuffer - нет?
+window.editor.addEventListener('update', e => {
+  // const ctx = e.canvas.getContext('2d');
+  // const image = ctx.getImageData(0, 0, e.canvas.width, e.canvas.height);
+  // const binary = Uint8Array.from(image.data);
+  // wss.send(binary.buffer);
+
+  e.canvas.toBlob(blob => wss.send(blob));
+});
