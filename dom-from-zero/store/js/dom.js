64b87624bc34470196b8cba9ece4e63f8ecdc87e
federikos <federikos@bk.ru>
2019-07-19 16:08:52 +0300

    add homework 4.3 Store

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function createElement(node) {
+  if(typeof node === 'string') {
+    return createTextNode(node);
+  }
+  const elem = document.createElement(node.name);
+
+  for (const key in node.props) {
+    elem.setAttribute(key, node.props[key]);
+  }
+
+  node.childs.forEach(child => {
+
+    if (typeof child === 'string') {
+      elem.textContent = child;
+    } 
+
+    if (typeof child === 'object') {
+      elem.appendChild(createElement(child));
+    }
+  });
+  return elem;
+};
